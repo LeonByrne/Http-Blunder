512508d6488c83606c18b13984ff99f041d1d6ea
Leon Byrne <leonbyrne52@gmail.com>
2024-08-15 16:05:52 +0100

    Sends a simple helloWorld test page for all requests.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+
+#include <fcntl.h>
+
+void send_page(int, const char *);
 
 int main()
 {
@@ -47,5 +54,29 @@ int main()
 	recv(client_fd, buffer, 256, 0);
 	printf("Request: %s\n", buffer);
 
+	send_page(client_fd, "resources/test.html");
+
 	return 0;
+}
+
+void send_page(int client_fd, const char *page)
+{
+	int page_fd = open(page, O_RDONLY);
+	struct stat page_stat;
+
+	fstat(page_fd, &page_stat);
+
+	char response[256];
+	snprintf(response, sizeof(response),
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Type: text/html\r\n"
+		"Content-Length: %ld\r\n"
+		"\r\n",
+		page_stat.st_size
+	);
+
+	send(client_fd, response, strlen(response), 0);
+	sendfile(client_fd, page_fd, NULL, page_stat.st_size);
+
+	close(page_fd);
 }
